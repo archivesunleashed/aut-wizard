6ada93708e029e62ee6a41e6bd79fb108cb85a09
Ryan Deschamps <ryan.deschamps@gmail.com>
2017-11-16 12:32:26 -0500

    Create basic filter templates. Remove using button. Styling to come later. TODO: * Drag and Drop filters * Control output format options based on what user wants to collect. * Input box for Collection path for now - later will be based on user development from auk.

--- build/aut-form.js
+++ build/aut-form.js
@@ -7,33 +7,129 @@ class AutForm {
     constructor() {
         var btn;
         var here = this;
+        var remove;
         document.addEventListener("DOMContentLoaded", function (doc) {
-            btn = document.getElementById('filter');
-            if (btn) {
-                btn.addEventListener("click", () => here.addFilter());
-            }
+            document.querySelector('body').addEventListener('click', function (event) {
+                var elem = event.target;
+                if (elem.className == 'remove') {
+                    here.removeFilter(elem.id);
+                }
+                switch (elem.id) {
+                    case 'filter': {
+                        here.addFilterForm();
+                        break;
+                    }
+                    case 'domain':
+                    case 'date':
+                    case 'url':
+                    case 'keyword':
+                        {
+                            here.addFilter(elem.id);
+                            break;
+                        }
+                    case 'language': {
+                        var langid = document.getElementById('languageinput');
+                        here.addFilter(elem.id, langid.options[langid.selectedIndex].value);
+                        break;
+                    }
+                    default: {
+                        // protect against misuse.
+                    }
+                }
+            });
         });
     }
-    addFilter() {
+    setAttributes(el, attrs) {
+        for (var key in attrs) {
+            el.setAttribute(key, attrs[key]);
+        }
+    }
+    addFilter(name, value = "") {
+        var filterform = document.getElementById('filterform');
+        let filters = document.getElementById('filters');
+        if (value == "") {
+            value = document.getElementById(name + 'input').value;
+        }
+        while (filterform.firstChild) {
+            filterform.removeChild(filterform.firstChild);
+        }
+        let div = filters.appendChild(document.createElement("div"));
+        let id = filters.getElementsByClassName('remove').length;
+        this.setAttributes(div, { "id": name + "filter",
+            "draggable": true });
+        div.innerHTML = "<br />Filter by " + name + ": " + value;
+        var button = div.appendChild(document.createElement('button'));
+        this.setAttributes(button, { "id": id,
+            "class": "remove" });
+        button.innerHTML = "X";
+    }
+    addFilterForm() {
         var select = document.getElementById('filter-type');
         var val = select.options[select.selectedIndex].value;
         switch (val) {
             case 'by domain': {
-                this.applyTemplate(`<div id="bydomain"><input>by domain </input>`);
+                this.applyTemplate(`<div id="bydomain">
+                            <input id="domaininput">by domain </input>
+                            <button id="domain">apply</button>
+                            </div>`);
                 break;
             }
             case 'by date range': {
-                this.applyTemplate('by date');
+                this.applyTemplate(`<div id="bydate">
+                            <input id="date" type="range" multiple value="1996,2016"
+                            min="1995" max="2017" step=1
+                            ></input>
+                          <button id="date"> apply</button>
+                        </div>`);
+                break;
+            }
+            case 'by url pattern': {
+                this.applyTemplate(`<div id="byurl">
+                            <input id="urlinput">by url pattern </input>
+                            <button id="url">apply</button>
+                            </div>`);
+                break;
+            }
+            case 'by language': {
+                this.applyTemplate(`<div id="bylanguage">
+                            <select id="languageinput">
+                            <option value="ar">ar / العربية</option>
+                            <option value="en">en / English</option>
+                            <option value="fr">fr / Français</option>
+                            <option value="ge">ge / Deutsche</option>
+                            <option value="zh">zh / 中国</option>
+                            </select>
+                            <button id="language">apply</button>
+                            </div>`);
+                break;
+            }
+            case 'keyword': {
+                this.applyTemplate(`<div id="keywords"><input id="keywordinput" placeholder="enter keywords...">Keyword filter</input>
+                            <button id="keyword">apply</button></div>`);
                 break;
             }
             default: {
-                this.applyTemplate('<p><i>Remove Boilerplate</i></p>');
+                var boilerexists = document.getElementById('boilerplate');
+                if (!boilerexists) {
+                    this.addFilter('boilerplate', 'remove boilerplate');
+                }
             }
         }
     }
+    removeFilter(id) {
+        let div = document.getElementById('filters');
+        div.removeChild(div.childNodes[Number(id)]);
+        let children = div.getElementsByClassName('remove');
+        for (var i = 0; i < children.length; i++) {
+            this.setAttributes(children[i], {
+                id: i,
+                class: 'remove'
+            });
+        }
+    }
     applyTemplate(template) {
-        var elem = document.getElementById("filters");
-        elem.appendChild(document.createElement('p')).innerHTML = template;
+        var elem = document.getElementById("filterform");
+        elem.innerHTML = template;
     }
 }
 ;
--- src/aut-form.ts
+++ src/aut-form.ts
@@ -50,38 +50,141 @@ class AutForm {
   constructor () {
     var btn:HTMLElement;
     var here = this;
+    var remove: HTMLElement;
 
     document.addEventListener("DOMContentLoaded", function(doc) {
-      btn = document.getElementById('filter');
-      if (btn) {
-        btn.addEventListener("click", () => here.addFilter());
-      }
-    }
-  );
+      document.querySelector('body').addEventListener('click', function(event) {
+          var elem = (<Element>event.target);
+          if (elem.className == 'remove') {
+            here.removeFilter(elem.id);
+          }
+          switch (elem.id) {
+            case  'filter' : {
+              here.addFilterForm();
+              break;
+          }
+            case 'domain' :
+            case 'date':
+            case 'url':
+            case 'keyword':
+            {
+              here.addFilter(elem.id)
+              break;
+            }
+            case 'language':{
+              var langid = (document.getElementById('languageinput') as HTMLSelectElement);
+              here.addFilter(elem.id, langid.options[langid.selectedIndex].value);
+              break;
+            }
+
+            default: {
+              // protect against misuse.
+            }
+
+          }
+
+          });
+  });
 
   }
 
-  addFilter () {
+  setAttributes(el:HTMLElement, attrs) {
+    for(var key in attrs) {
+      el.setAttribute(key, attrs[key]);
+    }
+  }
+
+  addFilter (name:string, value:string = "") {
+    var filterform = document.getElementById('filterform');
+    let filters = document.getElementById('filters');
+    if (value == "") {
+      value = (document.getElementById(name+'input') as HTMLInputElement).value;
+    }
+    while (filterform.firstChild) {
+      filterform.removeChild(filterform.firstChild);
+    }
+    let div = filters.appendChild(document.createElement("div"));
+    let id = filters.getElementsByClassName('remove').length
+    this.setAttributes(div,
+      {"id": name+"filter",
+      "draggable": true});
+    div.innerHTML = "<br />Filter by " + name + ": " + value;
+    var button = div.appendChild(document.createElement('button'));
+    this.setAttributes(button,
+      {"id": id,
+      "class": "remove"});
+    button.innerHTML = "X";
+  }
+
+  addFilterForm () {
     var select = document.getElementById('filter-type') as HTMLSelectElement
     var val = select.options[select.selectedIndex].value
     switch (val) {
       case 'by domain': {
-        this.applyTemplate(`<div id="bydomain"><input>by domain </input>`);
+        this.applyTemplate(`<div id="bydomain">
+                            <input id="domaininput">by domain </input>
+                            <button id="domain">apply</button>
+                            </div>`);
+        break;
+        }
+      case 'by date range' : {
+        this.applyTemplate(`<div id="bydate">
+                            <input id="date" type="range" multiple value="1996,2016"
+                            min="1995" max="2017" step=1
+                            ></input>
+                          <button id="date"> apply</button>
+                        </div>`);
         break;
       }
-      case 'by date range' : {
-        this.applyTemplate('by date');
+      case 'by url pattern' : {
+        this.applyTemplate(`<div id="byurl">
+                            <input id="urlinput">by url pattern </input>
+                            <button id="url">apply</button>
+                            </div>`);
+        break;
+      }
+      case 'by language' : {
+        this.applyTemplate(`<div id="bylanguage">
+                            <select id="languageinput">
+                            <option value="ar">ar / العربية</option>
+                            <option value="en">en / English</option>
+                            <option value="fr">fr / Français</option>
+                            <option value="ge">ge / Deutsche</option>
+                            <option value="zh">zh / 中国</option>
+                            </select>
+                            <button id="language">apply</button>
+                            </div>`);
+        break;
+      }
+      case 'keyword' : {
+        this.applyTemplate(`<div id="keywords"><input id="keywordinput" placeholder="enter keywords...">Keyword filter</input>
+                            <button id="keyword">apply</button></div>`);
         break;
       }
       default : {
-        this.applyTemplate ('<p><i>Remove Boilerplate</i></p>');
+        var boilerexists = document.getElementById('boilerplate');
+        if (!boilerexists){
+          this.addFilter ('boilerplate', 'remove boilerplate');
+        }
       }
     }
   }
 
+  removeFilter(id) {
+    let div = document.getElementById('filters')
+    div.removeChild(div.childNodes[Number(id)]);
+    let children = div.getElementsByClassName('remove');
+    for (var i = 0; i < children.length; i++) {
+      this.setAttributes((<HTMLElement>children[i]), {
+        id: i,
+        class: 'remove'
+      });
+    }
+  }
+
   applyTemplate(template:string) {
-     var elem = document.getElementById("filters");
-     elem.appendChild(document.createElement('p')).innerHTML = template;
+     var elem = document.getElementById("filterform");
+     elem.innerHTML = template;
   }
 };
 
